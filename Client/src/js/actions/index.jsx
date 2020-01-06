c289ef4ddd6b80038b3a63c4696c7b4b6a21faf0
Sorebit <cmbk90@gmail.com>
2020-01-06 10:43:39 +0100

    Move from thunk to saga

--- Client/src/js/actions/index.jsx
+++ Client/src/js/actions/index.jsx
@@ -1,19 +1,13 @@
-import { ADD_ARTICLE, DATA_LOADED } from "../constants/action-types";
+import { ADD_ARTICLE, DATA_LOADED, DATA_REQUESTED } from "../constants/action-types";
 
 export function addArticle(payload) {
   return { type: ADD_ARTICLE, payload };
 }
 
 export function dataLoaded(payload) {
-  return { type: DATA_LOADED, payload}
+  return { type: DATA_LOADED, payload };
 }
 
 export function getData() {
-  return function(dispatch) {
-    return fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(response => response.json())
-      .then(json => {
-        dispatch(dataLoaded(json));
-      });
-  };
+  return { type: DATA_REQUESTED };
 }
--- Client/src/js/store/index.jsx
+++ Client/src/js/store/index.jsx
@@ -1,13 +1,25 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
 import rootReducer from "../reducers/index";
 import { forbiddenWordsMiddleware } from "../middleware";
+import createSagaMiddleware from "redux-saga";
+import apiSaga from "../sagas/api-saga";
+
+const initialiseSagaMiddleware = createSagaMiddleware();
+// import reduxWebsocket from '@giantmachines/redux-websocket';
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+// Create the middleware instance.
+// const reduxWebsocketMiddleware = reduxWebsocket();
+
 const store = createStore(
   rootReducer,
-  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk)),
+  storeEnhancers(
+		// applyMiddleware(forbiddenWordsMiddleware, initialiseSagaMiddleware, reduxWebsocketMiddleware)
+		applyMiddleware(forbiddenWordsMiddleware, initialiseSagaMiddleware)
+	),
 );
+
+initialiseSagaMiddleware.run(apiSaga);
 
 export default store;
