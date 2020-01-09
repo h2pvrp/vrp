c470ec203968fddada70622a65551ab3c5f12e98
GorkyeMatt <gorkam@student.mini.pw.edu.pl>
2020-01-09 20:56:52 +0100

    Authorization

--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -5,13 +5,24 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import store from "./js/store/index";
 import App from "./js/components/App";
 import Register from "./js/components/Register";
+import { NavMenu } from "./js/components/NavMenu";
+// import AuthorizeRoute from "./js/components/api-authorization/AuthorizeRoute";
+import ApiAuthorizationRoutes from "./js/components/api-authorization/ApiAuthorizationRoutes";
+import { ApplicationPaths } from "./js/components/api-authorization/ApiAuthorizationConstants";
+
 import "bootstrap/dist/css/bootstrap.css";
 
 render(
   <Provider store={store}>
     <BrowserRouter>
+      <NavMenu />
       <Switch>
         <Route path="/register" component={Register} />
+        <Route
+          path={ApplicationPaths.ApiAuthorizationPrefix}
+          component={ApiAuthorizationRoutes}
+        />
+        <Route path="/login" component={ApiAuthorizationRoutes} />
         <Route path="/" component={App} /> {/**this have to be last route */}
       </Switch>
     </BrowserRouter>
